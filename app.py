f39619ec9805fb00c111b4a7e6a41f31c391663a
Anas Abu Farraj <anas.r.abufarraj@gmail.com>
2019-11-26 10:25:38 +0200

    Backup project

--- app.py
+++ app.py
@@ -2,4 +2,28 @@
 # ------------------------------------------------------------------------------
 #  Copyright (c) 2019. Anas Abu Farraj
 # ------------------------------------------------------------------------------
+""" Learning Flask-RESTful extension"""
 
+from flask import Flask
+from flask_restful import Resource, Api
+
+app = Flask(__name__)
+api = Api(app)
+
+
+class Student(Resource):
+    """Define Student class."""
+    @staticmethod
+    def get(name):
+        """Returns name.
+
+        :param name: string
+        :return: a dictionary of student name
+        """
+        return {'student': name}
+
+
+api.add_resource(Student, '/student/<string:name>')
+
+if __name__ == '__main__':
+    app.run()
