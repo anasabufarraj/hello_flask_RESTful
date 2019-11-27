ec5ef7185079e49f00d2c1f9ad9aa7a354124407
Anas Abu Farraj <anas.r.abufarraj@gmail.com>
2019-11-27 22:56:39 +0200

    Backup project

--- app.py
+++ app.py
@@ -4,26 +4,53 @@
 # ------------------------------------------------------------------------------
 """ Learning Flask-RESTful extension"""
 
-from flask import Flask
+from flask import Flask, request
 from flask_restful import Resource, Api
 
-app = Flask(__name__)
-api = Api(app)
+APP = Flask(__name__)
+API = Api(APP)
+
+ITEMS = [{'name': 'book', 'price': 17.99}]
 
 
-class Student(Resource):
-    """Define Student class."""
+class ItemList(Resource):
+    @staticmethod
+    def get():
+        """Returns a JSON of all items with 200 (OK) response."""
+        return {'items': ITEMS}, 200
+
+
+class Item(Resource):
     @staticmethod
     def get(name):
-        """Returns name.
-
+        """Returns item by name if found.
+        Iterate in lazy way over a filter object with next, return an item if found,
+        or return None by default.
         :param name: string
-        :return: a dictionary of student name
+        :returns: {"item": <name>} or {"item": null}, 200 if True, else 404.
         """
-        return {'student': name}
+        item = next(filter(lambda x: x['name'] == name, ITEMS), None)
+        return {'item': item}, 200 if item else 404
+
+    @staticmethod
+    def post(name):
+        """Create and append new item, returns a message if exist and 400 (BAD REQUEST).
+        :param name: strong
+        :returns: item and 201 (CREATED)
+        """
+        item = next(filter(lambda x: x['name'] == name, ITEMS), None)
+        if item:
+            return {'message': f'item with name {name} already exists!'}, 400
+
+        request_data = request.get_json()  # Data posts in the request bod
+        item = {'name': name, 'price': request_data['price']}
+        ITEMS.append(item)
+
+        return item, 201
 
 
-api.add_resource(Student, '/student/<string:name>')
+API.add_resource(ItemList, '/item')
+API.add_resource(Item, '/item/<string:name>')
 
 if __name__ == '__main__':
-    app.run()
+    APP.run()
